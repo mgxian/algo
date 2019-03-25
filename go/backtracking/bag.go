c5bad23eb587cea153c29e8ca09dfe3bc1dba9c1
will <maoguangxian@famulei.com>
2019-03-25 18:29:00 +0800

    zero one bag with cache

--- go/backtracking/bag.go
+++ go/backtracking/bag.go
@@ -14,3 +14,23 @@ func zeroOneBag(i, cw int, items []int, lw int, mw *int) {
 		zeroOneBag(i+1, cw+items[i], items, lw, mw)
 	}
 }
+
+func zeroOneBagWithCache(i, cw int, items []int, lw int, mw *int, cache [][]bool) {
+	length := len(items)
+	if i == length || cw == lw {
+		if cw > *mw {
+			*mw = cw
+		}
+		return
+	}
+
+	if cache[i][cw] == true {
+		return
+	}
+
+	cache[i][cw] = true
+	zeroOneBagWithCache(i+1, cw, items, lw, mw, cache)
+	if cw+items[i] <= lw {
+		zeroOneBagWithCache(i+1, cw+items[i], items, lw, mw, cache)
+	}
+}
--- go/backtracking/bag_test.go
+++ go/backtracking/bag_test.go
@@ -3,9 +3,21 @@ package backtracking
 import "testing"
 
 func TestZeroOneBag(t *testing.T) {
-	items := []int{9, 3, 6, 7}
-	limitedWeight := 20
+	items := []int{2, 2, 4, 6, 3}
+	limitedWeight := 9
 	maxWeight := -1
 	zeroOneBag(0, 0, items, limitedWeight, &maxWeight)
+	t.Log(maxWeight)
+}
+
+func TestZeroOneBagWithCache(t *testing.T) {
+	items := []int{2, 2, 4, 6, 3}
+	limitedWeight := 9
+	maxWeight := -1
+	cache := make([][]bool, len(items))
+	for i := range cache {
+		cache[i] = make([]bool, limitedWeight+1)
+	}
+	zeroOneBagWithCache(0, 0, items, limitedWeight, &maxWeight, cache)
 	t.Log(maxWeight)
 }
