db0aed6067cc7b7bde6184fce9495a381973a1a9
will <maoguangxian@famulei.com>
2019-04-22 18:51:39 +0800

    practice add circular doubly linked list

--- go/practice/list/circular.go
+++ go/practice/list/circular.go
@@ -1,16 +1,121 @@
 package list
 
-// CircularLinkedList represents a singly linked list.
-type CircularLinkedList struct {
+// CircularDoublyLinkedList represents a singly linked list.
+type CircularDoublyLinkedList struct {
 	head Element // sentinel list element
 	len  int     // current list length excluding (this) sentinel element
 }
 
-// NewCircularLinkedList create a new initial CircularLinkedList
-func NewCircularLinkedList() *CircularLinkedList {
-	l := new(CircularLinkedList)
+// NewCircularDoublyLinkedList create a new initial CircularDoublyLinkedList
+func NewCircularDoublyLinkedList() *CircularDoublyLinkedList {
+	l := new(CircularDoublyLinkedList)
 	l.head.prev = &l.head
 	l.head.next = &l.head
 	l.len = 0
 	return l
+}
+
+// Front returns the first element of the linked list or nil if the linked list is empty.
+func (l *CircularDoublyLinkedList) Front() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.head.next
+}
+
+// Back returns the last element of the linked list or nil if the linked list is empty.
+func (l *CircularDoublyLinkedList) Back() *Element {
+	if l.len == 0 {
+		return nil
+	}
+	return l.head.prev
+}
+
+// Len returns the number of elements of the linked list.
+func (l *CircularDoublyLinkedList) Len() int {
+	return l.len
+}
+
+func (l *CircularDoublyLinkedList) insert(e, at *Element) *Element {
+	n := at.next
+	at.next = e
+	e.prev = at
+
+	e.next = n
+	n.prev = e
+
+	l.len++
+	return e
+}
+
+func (l *CircularDoublyLinkedList) remove(e *Element) *Element {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+
+	l.len--
+	return e
+}
+
+func (l *CircularDoublyLinkedList) move(e, at *Element) *Element {
+	if e == at {
+		return e
+	}
+
+	e.prev.next = e.next
+	e.next.prev = e.prev
+
+	n := at.next
+	at.next = e
+	e.prev = at
+
+	e.next = n
+	n.prev = e
+
+	return e
+}
+
+func (l *CircularDoublyLinkedList) insertValue(v interface{}, at *Element) *Element {
+	return l.insert(&Element{Value: v}, at)
+}
+
+// InsertAfter inserts a new element e with value v after mark and returns e.
+func (l *CircularDoublyLinkedList) InsertAfter(v interface{}, mark *Element) *Element {
+	return l.insertValue(v, mark)
+}
+
+// InsertBefore inserts a new element e with value v before mark and returns e.
+func (l *CircularDoublyLinkedList) InsertBefore(v interface{}, mark *Element) *Element {
+	return l.insertValue(v, mark.prev)
+}
+
+// MoveToBack moves element e to the back of the linked list.
+func (l *CircularDoublyLinkedList) MoveToBack(e *Element) {
+	if l.head.prev == e {
+		return
+	}
+	l.move(e, l.head.prev)
+}
+
+// MoveToFront moves element e to the front of the linked list.
+func (l *CircularDoublyLinkedList) MoveToFront(e *Element) {
+	if l.head.next == e {
+		return
+	}
+	l.move(e, &l.head)
+}
+
+// PushBack inserts a new element e with value v to the back of the linked list.
+func (l *CircularDoublyLinkedList) PushBack(v interface{}) *Element {
+	return l.insertValue(v, l.head.prev)
+}
+
+// PushFront inserts a new element e with value v to the front of the linked list.
+func (l *CircularDoublyLinkedList) PushFront(v interface{}) *Element {
+	return l.insertValue(v, &l.head)
+}
+
+// Remove remove element e from the linked list and returns the element value.
+func (l *CircularDoublyLinkedList) Remove(e *Element) interface{} {
+	l.remove(e)
+	return e.Value
 }
---
+++ go/practice/list/circular_test.go
@@ -0,0 +1,294 @@
+package list
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func checkCircularDoublyLinkedListLen(l *CircularDoublyLinkedList, es []*Element) bool {
+	return l.Len() == len(es)
+}
+
+func checkCircularDoublyLinkedListNextPointers(l *CircularDoublyLinkedList, es []*Element) bool {
+	prev := &l.head
+	for _, e := range es {
+		if prev.next != e {
+			return false
+		}
+		prev = prev.next
+	}
+
+	back := es[len(es)-1]
+	if back.next != &l.head {
+		return false
+	}
+
+	return true
+}
+
+func checkCircularDoublyLinkedListPrevPointers(l *CircularDoublyLinkedList, es []*Element) bool {
+	prev := &l.head
+	for _, e := range es {
+		if e.prev != prev {
+			fmt.Println("middle")
+			return false
+		}
+		prev = prev.next
+	}
+
+	back := es[len(es)-1]
+	if l.head.prev != back {
+		return false
+	}
+
+	return true
+}
+
+func checkEmptyCircularDoublyLinkedListPointers(l *CircularDoublyLinkedList) bool {
+	if (l.head.next != nil && l.head.next != &l.head) || (l.head.prev != nil && l.head.prev != &l.head) {
+		return false
+	}
+	return true
+}
+
+func checkCircularDoublyLinkedListPointers(l *CircularDoublyLinkedList, es []*Element) bool {
+	if !checkCircularDoublyLinkedListLen(l, es) {
+		fmt.Println("len")
+		return false
+	}
+
+	if len(es) == 0 {
+		return checkEmptyCircularDoublyLinkedListPointers(l)
+	}
+
+	if !checkCircularDoublyLinkedListNextPointers(l, es) {
+		fmt.Println("next")
+		return false
+	}
+
+	if !checkCircularDoublyLinkedListPrevPointers(l, es) {
+		fmt.Println("prev")
+		return false
+	}
+
+	return true
+}
+
+func TestCircularDoublyLinkedListPushBack(t *testing.T) {
+	Convey("Test doubly linked list PushBack", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test empty doubly linked list", func() {
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{}), ShouldEqual, true)
+			})
+
+			Convey("Test push back single element doubly linked list", func() {
+				e := l.PushBack(1)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test push back more elements doubly linked list", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e1, e2, e3}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListPushFront(t *testing.T) {
+	Convey("Test doubly linked list PushFront", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test push front single element doubly linked list", func() {
+				e := l.PushFront(1)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test push front more elements doubly linked list", func() {
+				e1 := l.PushFront(1)
+				e2 := l.PushFront(2)
+				e3 := l.PushFront(3)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e3, e2, e1}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListFrontBack(t *testing.T) {
+	Convey("Test doubly linked list Front and Back", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test empty doubly linked list", func() {
+				So(l.Front(), ShouldBeNil)
+				So(l.Back(), ShouldBeNil)
+			})
+
+			Convey("Test push front single element doubly linked list", func() {
+				e := l.PushFront(1)
+				So(l.Front(), ShouldEqual, e)
+				So(l.Back(), ShouldEqual, e)
+			})
+
+			Convey("Test push back single element doubly linked list", func() {
+				e := l.PushBack(1)
+				So(l.Front(), ShouldEqual, e)
+				So(l.Back(), ShouldEqual, e)
+			})
+
+			Convey("Test push front more elements doubly linked list", func() {
+				e1 := l.PushFront(1)
+				l.PushFront(2)
+				e3 := l.PushFront(3)
+				So(l.Front(), ShouldEqual, e3)
+				So(l.Back(), ShouldEqual, e1)
+			})
+
+			Convey("Test push back more elements doubly linked list", func() {
+				e1 := l.PushBack(1)
+				l.PushBack(2)
+				e3 := l.PushBack(3)
+				So(l.Front(), ShouldEqual, e1)
+				So(l.Back(), ShouldEqual, e3)
+			})
+
+			Convey("Test push front and back more elements doubly linked list", func() {
+				l.PushFront(1)
+				l.PushFront(2)
+				l.PushFront(3)
+				e4 := l.PushBack(4)
+				e5 := l.PushFront(5)
+				So(l.Front(), ShouldEqual, e5)
+				So(l.Back(), ShouldEqual, e4)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListInsertAfter(t *testing.T) {
+	Convey("Test doubly linked list InsertAfter", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test InsertAfter single element doubly linked list", func() {
+				e := l.InsertAfter(1, &l.head)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test InsertAfter more elements doubly linked list", func() {
+				e1 := l.InsertAfter(1, &l.head)
+				e2 := l.InsertAfter(2, e1)
+				e3 := l.InsertAfter(3, e2)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e1, e2, e3}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListInsertBefore(t *testing.T) {
+	Convey("Test doubly linked list InsertBefore", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test InsertBefore single element doubly linked list", func() {
+				e := l.InsertBefore(1, &l.head)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test InsertBefore more elements doubly linked list", func() {
+				e1 := l.InsertAfter(1, &l.head)
+				e2 := l.InsertBefore(2, e1)
+				e3 := l.InsertBefore(3, e2)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e3, e2, e1}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListMoveToBack(t *testing.T) {
+	Convey("Test doubly linked list MoveToBack", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test single element doubly linked list", func() {
+				e := l.PushBack(1)
+				l.MoveToBack(e)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test more elements doubly linked list", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				l.MoveToBack(e1)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e2, e3, e1}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListMoveToFront(t *testing.T) {
+	Convey("Test doubly linked list MoveToFront", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test single element doubly linked list", func() {
+				e := l.PushBack(1)
+				l.MoveToFront(e)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e}), ShouldEqual, true)
+			})
+
+			Convey("Test more elements doubly linked list", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				l.MoveToFront(e3)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e3, e1, e2}), ShouldEqual, true)
+			})
+		})
+	})
+}
+
+func TestCircularDoublyLinkedListRemove(t *testing.T) {
+	Convey("Test doubly linked list Remove", t, func() {
+		Convey("Setup", func() {
+			l := NewCircularDoublyLinkedList()
+
+			Convey("Test single element doubly linked list", func() {
+				e := l.PushBack(1)
+				l.Remove(e)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{}), ShouldEqual, true)
+			})
+
+			Convey("Test more elements doubly linked list remove first element", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				l.Remove(e1)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e2, e3}), ShouldEqual, true)
+			})
+
+			Convey("Test more elements doubly linked list remove last element", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				l.Remove(e3)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e1, e2}), ShouldEqual, true)
+			})
+
+			Convey("Test more elements doubly linked list remove middle element", func() {
+				e1 := l.PushBack(1)
+				e2 := l.PushBack(2)
+				e3 := l.PushBack(3)
+				l.Remove(e2)
+				So(checkCircularDoublyLinkedListPointers(l, []*Element{e1, e3}), ShouldEqual, true)
+			})
+		})
+	})
+}
