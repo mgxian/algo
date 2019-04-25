3aa2984c85f6af6dfe77ecdaed783450e6a4bdd8
will <maoguangxian@famulei.com>
2019-04-25 15:32:28 +0800

    practice add queue interface

--- go/practice/queue/queue.go
+++ go/practice/queue/queue.go
@@ -2,9 +2,14 @@ package queue
 
 // Queue is a interface that a queue must implements.
 type Queue interface {
-	// InQueue inqueue a element to the tail of queue
-	InQueue(e interface{}) interface{}
-	DeQueue() interface{}
+	// Enqueue add element to end of queue.
+	Enqueue(e interface{}) interface{}
+	// Dequeue remove element from front of queue.
+	Dequeue() interface{}
+	// IsEmpty check if queue is empty.
 	IsEmpty() bool
+	// IsFull check if queue is full.
 	IsFull() bool
+	// Peek get the value of the front of queue without removing it.
+	Peek() interface{}
 }
