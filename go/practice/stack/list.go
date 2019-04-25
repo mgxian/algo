3623a5eb8024854cd54f956c02d13a3254a0687f
will <maoguangxian@famulei.com>
2019-04-25 14:50:06 +0800

    practice add list stack

---
+++ go/practice/stack/list.go
@@ -0,0 +1,64 @@
+package stack
+
+import (
+	"github.com/mgxian/algo/go/practice/list"
+)
+
+// ListStack represents a list stack.
+type ListStack struct {
+	capacity int
+	l        list.List
+}
+
+// NewListStack creates a new initial list stack of n elements.
+func NewListStack(n int) *ListStack {
+	if n < 1 {
+		return nil
+	}
+	return &ListStack{
+		capacity: n,
+		l:        list.NewSinglyLinkedList(),
+	}
+}
+
+// Push add a element to top of stack.
+func (s *ListStack) Push(e interface{}) interface{} {
+	if s.IsFull() {
+		return nil
+	}
+	s.l.PushBack(e)
+	return e
+}
+
+// Pop remove a element from top of stack.
+func (s *ListStack) Pop() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	if result := s.l.Back(); result != nil {
+		s.l.Remove(result)
+		return result.Value
+	}
+	return nil
+}
+
+// IsEmpty check if stack is empty.
+func (s *ListStack) IsEmpty() bool {
+	return s.l.Len() == 0
+}
+
+// IsFull check if stack is full.
+func (s *ListStack) IsFull() bool {
+	return s.l.Len() == s.capacity
+}
+
+// Peek get the value of the top element without removing it.
+func (s *ListStack) Peek() interface{} {
+	if s.IsEmpty() {
+		return nil
+	}
+	if result := s.l.Back(); result != nil {
+		return result.Value
+	}
+	return nil
+}
