a6405f1656675c04086891065028639fd541e43c
mgxian <will835559313@163.com>
2019-05-13 14:46:04 +0800

    practice binary search have same item

--- go/practice/bsearch/bsearch.go
+++ go/practice/bsearch/bsearch.go
@@ -16,3 +16,43 @@ func binarySearch(data []int, num int) int {
 	}
 	return -1
 }
+
+func binarySearchFirstEqual(data []int, num int) int {
+	low := 0
+	high := len(data) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] > num {
+			high = mid - 1
+		} else if data[mid] < num {
+			low = mid + 1
+		} else {
+			if mid == 0 || data[mid-1] != num {
+				return mid
+			}
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
+func binarySearchLastEqual(data []int, num int) int {
+	low := 0
+	high := len(data) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+		if data[mid] > num {
+			high = mid - 1
+		} else if data[mid] < num {
+			low = mid + 1
+		} else {
+			if mid == len(data)-1 || data[mid+1] != num {
+				return mid
+			}
+			low = mid + 1
+		}
+	}
+	return -1
+}
--- go/practice/bsearch/bsearch_test.go
+++ go/practice/bsearch/bsearch_test.go
@@ -21,3 +21,35 @@ func TestBinarySearch(t *testing.T) {
 	i = binarySearch(nums, 30)
 	assert.Equal(t, -1, i)
 }
+
+func TestBinarySearchFirstEqual(t *testing.T) {
+	nums := []int{7, 9, 10, 11, 11, 11, 12, 15, 20}
+
+	i := binarySearchFirstEqual(nums, 7)
+	assert.Equal(t, 0, i)
+
+	i = binarySearchFirstEqual(nums, 11)
+	assert.Equal(t, 3, i)
+
+	i = binarySearchFirstEqual(nums, 20)
+	assert.Equal(t, 8, i)
+
+	i = binarySearchFirstEqual(nums, 30)
+	assert.Equal(t, -1, i)
+}
+
+func TestBinarySearchLastEqual(t *testing.T) {
+	nums := []int{7, 9, 10, 11, 11, 11, 12, 15, 20}
+
+	i := binarySearchLastEqual(nums, 7)
+	assert.Equal(t, 0, i)
+
+	i = binarySearchLastEqual(nums, 11)
+	assert.Equal(t, 5, i)
+
+	i = binarySearchLastEqual(nums, 20)
+	assert.Equal(t, 8, i)
+
+	i = binarySearchLastEqual(nums, 30)
+	assert.Equal(t, -1, i)
+}
