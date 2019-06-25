2249555c279ed9e4b878dfbfbaf858fe0aed1c85
mgxian <will835559313@163.com>
2019-06-25 16:03:58 +0800

    practice topological sort with dfs algorithm

--- go/practice/graph/topoSort.go
+++ go/practice/graph/topoSort.go
@@ -3,6 +3,8 @@ package graph
 import (
 	"strconv"
 	"strings"
+
+	"github.com/mgxian/algo/go/practice/list"
 )
 
 func topologicalSortUseKahnAlgorithm(graph *directedWeightedGraph) string {
@@ -50,6 +52,52 @@ func topologicalSortUseKahnAlgorithm(graph *directedWeightedGraph) string {
 		visited[node] = true
 		result.WriteString(" " + strconv.Itoa(node))
 		doneNode(node)
+	}
+
+	if strings.HasPrefix(result.String(), " ") {
+		return result.String()[1:]
+	}
+	return result.String()
+}
+
+func dfs(node int, reversedAjacencyList map[int]list.List, visited map[int]bool, output *strings.Builder) {
+	if visited[node] {
+		return
+	}
+
+	p := reversedAjacencyList[node].Front()
+	for p != nil {
+		e := p.Value.(edge)
+		if !visited[e.destnation] {
+			dfs(e.destnation, reversedAjacencyList, visited, output)
+		}
+		p = p.Next()
+	}
+
+	visited[node] = true
+	output.WriteString(" " + strconv.Itoa(node))
+}
+
+func topologicalSortUseDFSAlgorithm(graph *directedWeightedGraph) string {
+	result := new(strings.Builder)
+	visited := make(map[int]bool, len(graph.adjacencyList))
+	reversedAjacencyList := make(map[int]list.List, len(graph.adjacencyList))
+	for i := 0; i < len(graph.adjacencyList); i++ {
+		reversedAjacencyList[i] = list.NewSinglyLinkedList()
+		visited[i] = false
+	}
+
+	for node, edges := range graph.adjacencyList {
+		p := edges.Front()
+		for p != nil {
+			e := p.Value.(edge)
+			reversedAjacencyList[e.destnation].PushBack(edge{e.destnation, node, -1})
+			p = p.Next()
+		}
+	}
+
+	for i := 0; i < len(graph.adjacencyList); i++ {
+		dfs(i, reversedAjacencyList, visited, result)
 	}
 
 	if strings.HasPrefix(result.String(), " ") {
